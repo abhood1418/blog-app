88b48debf74e89a63ba724f4bd92e479b4795ce1
Darryl Falls <darryl.a.falls@gmial.com>
2021-11-09 11:49:19 -0500

    added signIn and signUp

--- client/src/screens/SignIn/SignIn.jsx
+++ client/src/screens/SignIn/SignIn.jsx
@@ -1,10 +1,69 @@
 // Darryl
+import { useState, useEffect } from 'react'
+import { getUser, getUsers } from '../../services/users'
 
+const SignIn = ({user, setUser, loggedIn, setLoggedIn}) => {
+  const [usernameInput, setUsernameInput] = useState('')
+  const [passwordInput, setPasswordInput] = useState('')
 
-const SignIn = () => {
+  const setLocalStorage = () => {
+    localStorage.setItem("userID", `${user._id}`)
+  }
+
+  const checkForUser = async () => {
+    const res = await getUsers()
+    let account = res.data.find((user) => user.username === usernameInput)
+    if (account === undefined) {
+      alert('Sorry. Your username and/or password are incorrect')
+    } else {
+      if (account.password === passwordInput) {
+        setUser(account)
+          .then(
+            setLoggedIn(true),
+            setLocalStorage())
+      } else {
+        alert('Sorry. Your username and/or password are incorrect')
+      }
+    }
+  }
+
+  const handleSubmit = async (ev) => {
+    ev.preventDefault()
+    checkForUser()
+  }
+
+  const checkLocalStorage = async () => {
+    const id = localStorage.getItem("userID")
+    if (id.length > 5) {
+      let res = await getUser(id)
+      setUser(res.data)
+      setLoggedIn(true)
+    }
+  }
+
+  useEffect(() => {
+    checkLocalStorage()
+  },[])
+
   return (
     <div>
-      
+      <form onSubmit={handleSubmit(ev)}>
+        <label>Username: </label>
+        <input placeHolder='Username' value={usernameInput} onChange={(ev) => setUsernameInput(ev.target.value)} />
+        <br />
+        <label>Password: </label>
+        <input type='password' placeHolder='Password' value={passwordInput} onChange={(ev) => setPasswordInput(ev.target.value)} />
+        <br />
+        <input type='submit'/>
+      </form>
+      <br />
+      <div>
+        If you are new to this site, you can create a new account by clicking the link below.
+      </div>
+      <br />
+      <Link to='/user-create'><div>Create New Account</div></Link>
     </div>
   )
 }
+
+export default SignIn
--- client/src/screens/SignUp/SignUp.jsx
+++ client/src/screens/SignUp/SignUp.jsx
@@ -1,1 +1,80 @@
 // Darryl
+import { useState } from 'react'
+import {Link, Redirect} from 'react-router-dom'
+import { createUser, getUsers } from '../../services/users'
+
+const SignUp = ({setUser, setLoggedIn}) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [name, setName] = useState('')
+  const [imgURL, setImgURL] = useState('')
+  const [redirect, setRedirect] = useState(false)
+
+  const setLocalStorage = () => {
+    localStorage.setItem("userID", `${user._id}`)
+  }
+
+  const newUser = async () => {
+    let user = {
+      username,
+      name,
+      imgURL,
+      password
+    }
+    const newUserAccount = await createUser(user)
+    setUser(newUserAccount)
+      .then(
+      setLocalStorage()
+    )
+  }
+
+  const check = (nameCheck) => {
+    if (nameCheck === undefined) {
+      newUser()
+      alert('Congratulations! You now have an account')
+      setLoggedIn(true)
+      setRedirect(true)
+    } else {
+      alert('Username is already taken. Please choose a new username.')
+    }
+  }
+
+  const checkUsername = async () => {
+    const res = await getUsers()
+    const nameCheck = res.data.find((user) => user.username === username)
+      .then(check(nameCheck))
+  }
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault()
+    checkUsername()
+  }
+
+  if (redirect === true) {
+    return <Redirect to='/'/>
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(ev)}>
+        <label>Username: </label>
+        <input placeHolder='Username' value={username} onChange={(ev) => setUsername(ev.target.value)} />
+        <br />
+        <label>Password: </label>
+        <input type='password' placeHolder='Password' value={password} onChange={(ev) => setPassword(ev.target.value)} />
+        <br />
+        <label>Name: </label>
+        <input placeHolder='Name' value={name} onchange={(ev) => setName(ev.target.value)}/>
+        <br/>
+        <label>Profile Picture: </label>
+        <input placeHolder='Image URL' value={imgURL} onChange={(ev) => setImgURL(ev.target.value)}/>
+        <br/>
+        <input type='submit'/>
+      </form>
+      <br />
+      <Link to='/'><div>Back to Sign In</div></Link>
+    </div>
+  )
+}
+
+export default SignUp

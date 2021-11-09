80957a2e508e560aba7d580dfa1c804615f3a16c
Terra <bclaycamp@gmail.com>
2021-11-08 21:47:31 -0500

    nav footer comments layedout

--- client/src/components/Comments/Comments.jsx
+++ client/src/components/Comments/Comments.jsx
@@ -1,1 +1,21 @@
-// Terra
+// Terra Claycamp
+
+function Comments(props) {
+	
+	return (
+    <div className="comments-section">
+      <h3>Comments</h3>
+      <ul>
+        {gamePosts.map((gamePosts) => (
+          <Link
+            to={`/gamePosts/${gamePosts.id}`}
+            key={gamePosts.id}
+            className="post-Links"
+          >
+            <li>{gamePosts.fields.playerName}</li>
+          </Link>
+        ))}
+      </ul>
+    </div>
+  );
+}
--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,1 +1,15 @@
-// Terra 
+// Terra Claycamp
+import React from "react"
+import { Footer } from "../Footer/Footer"
+import { Nav } from "../Nav/Nav"
+
+function Layout(props) {
+	
+
+	return (
+		<div>
+			<Nav />
+			<Footer />
+		</div>
+	)
+}

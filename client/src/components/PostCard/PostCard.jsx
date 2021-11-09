fd00c92e6600c5ff57be16bb3c888ee3b30752ae
Andrew Freedman <ajfreedm@gmail.com>
2021-11-09 11:32:24 -0500

    added posts

--- client/src/components/PostCard/PostCard.jsx
+++ client/src/components/PostCard/PostCard.jsx
@@ -1,1 +1,44 @@
-// Andrew F.
+import './PostCard.css';
+import { Link } from 'react-router-dom'
+
+const PostCard = (props) => {
+    return (
+        <div className="post-card">
+            <Link className="card" to={`/posts/${props._id}`}>
+            
+            <div>{props.username}</div> 
+            <div>{props.title}</div> 
+            <img  src={props.imgURL} alt={props.title} /> 
+            <div>{props.content}</div> 
+            <div>{props.hashtags}</div> 
+            <div>{props.comments}</div> 
+
+
+             {/* <Commments data={}/> */}
+
+
+                {/* <img className="post-card-image" src={props.imgURL} alt={props.name} /> */}
+                <div>View</div>
+            </Link>
+        </div>
+    )
+}
+
+// {
+//     username: { type: String, required: true },
+//     title: { type: String, required: true },
+//     imgURL: { type: String},
+//     content: { type: String, required: true },
+//     hashtags: [{ type: String }],
+//     comments: [{
+//       user: { type: String },
+//       userImgURL: { type: String },
+//       comment: { type: String },
+//     },]
+//   },
+//   { timestamps: true }
+// )
+
+
+
+export default PostCard
--- client/src/screens/Posts/Posts.jsx
+++ client/src/screens/Posts/Posts.jsx
@@ -1,9 +1,43 @@
+import { useState, useEffect } from 'react'
+import './Posts.css'
 
+import PostCard from '../../components/PostCard/PostCard'
+import Layout from '../../components/Layout/Layout'
+import { getPosts} from '../../services/posts'
 
 const Posts = () => {
+  const [posts, setPosts] = useState([])
+
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const allPosts = await getPosts()
+      setPosts(allPosts)
+    }
+    fetchPosts()
+  }, [])
+
+
   return (
-    <h1>This is Posts</h1>
+    <Layout>
+      <div className='posts'>
+        {searchResult.map((post, index) => {
+          return (
+            <Post
+            _id={post._id}
+            username={post.username}
+            title={post.title}
+            imgURL={post.imgURL}
+            content={post.content}
+            hashtags={post.hashtags}
+            comments={post.comments}
+            key={index}
+            />
+          )
+        })}
+      </div>
+    </Layout>
   )
 }
 
-export default Posts;
+export default Posts
